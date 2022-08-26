42faf458482879f714fc6145161958ab6a25cabd
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-08-26 15:55:05 -0500

    Modal open close

--- script.js
+++ script.js
@@ -3,13 +3,19 @@
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
-const btnShowModal = document.querySelectorAll('.show-modal')
+const btnsShowModal = document.querySelectorAll('.show-modal')
 
-console.log(btnShowModal)
-
-for (let i=0; i<btnShowModal.length ; i++){
-    console.log(btnShowModal[i].textContent)
+const openModal = function () {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+const closeModal = function () {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
 }
 
-
-
+for (let i = 0; i < btnsShowModal.length; i++) {
+    btnsShowModal[i].addEventListener('click', openModal)
+    btnCloseModal.addEventListener('click', closeModal)
+    overlay.addEventListener('click', closeModal)
+}
